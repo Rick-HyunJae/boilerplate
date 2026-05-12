9aedf5f27298833c26d54d29268e09d7d7263fd3
조현재 <rickyHJ1994@gmail.com>
2026-05-12 18:19:15 +0900

    feat(api): rewrite shared/api entry point with namespace exports

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,1 +1,23 @@
-export { apiClient } from './client';
+export * as axiosApi from './axios/apiControllers';
+export * as axiosHandlers from './axios/apiHandlers';
+
+export * as fetchApi from './fetch';
+
+export { eventSourceController } from './eventSource';
+export type {
+    IEventSourcePolyfill,
+    IEventSourcePolyfillErrorEvent,
+    IEventSourcePolyfillMessageEvent,
+    IEventSourcePolyfillOpenEvent,
+    IEventSourcePolyfillOption,
+} from './eventSource';
+
+export { apiUrl, unCertUrl, objectStorageUrl, dwUrl } from './config';
+export type { TController, TRequestHeader } from './types';
+export {
+    createRequestHeader,
+    createUncertSignature,
+    downloadFileFromS3,
+    wrapPromise,
+    createEncryptServiceKey,
+} from './util';
