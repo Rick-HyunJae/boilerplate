39603f7d787d8e274a0b7b7ba598e84bf704d16f
조현재 <rickyHJ1994@gmail.com>
2026-05-12 18:09:38 +0900

    feat(api): port shared api config, types, util

---
+++ src/shared/api/config.ts
@@ -0,0 +1,37 @@
+// javascript-obfuscator:disable
+import { ENV } from '@/shared/config/env';
+
+const isLive = ENV.BUILD_TYPE === 'live';
+
+function generateGWUrl() {
+    switch (ENV.SERVICE_TYPE) {
+        case 'wehagom':
+            return 'https://api.wehagom.com';
+        case 'wehagov':
+            return 'https://api.wehagov.com';
+        case 'aws':
+            return 'https://api.insightofus.ai';
+        default:
+            return 'https://api.wehago.com';
+    }
+}
+
+function generateUnCertUrl() {
+    switch (ENV.SERVICE_TYPE) {
+        case 'wehagom':
+            return 'https://api0.wehagom.com';
+        case 'wehagov':
+            return 'https://api0.wehagov.com';
+        case 'aws':
+            return 'https://api0.insightofus.ai';
+        default:
+            return 'https://api0.wehago.com';
+    }
+}
+
+const apiUrl = isLive ? generateGWUrl() : 'http://dev.api.wehago.com';
+const unCertUrl = isLive ? generateUnCertUrl() : 'http://dev.api0.wehago.com';
+const objectStorageUrl = apiUrl + '/ObjectStorageCommon/services/common';
+const dwUrl = 'https://dwapi.wehago.com';
+
+export { apiUrl, unCertUrl, objectStorageUrl, dwUrl };
