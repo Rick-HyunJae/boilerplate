a172062ec196929cea3991fb959f8808d6814be7
조현재 <rickyHJ1994@gmail.com>
2026-05-12 18:14:56 +0900

    feat(api): port eventSource controller and polyfill

---
+++ src/shared/api/eventSource/eventsource.d.ts
@@ -0,0 +1,1 @@
+export const EventSourcePolyfill: any;
---
+++ src/shared/api/eventSource/types.ts
@@ -0,0 +1,47 @@
+/**
+ * @description EventSourcePolyfill 생성자 옵션
+ */
+export interface IEventSourcePolyfillOption {
+    method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
+    headers?: Record<string, string | number>;
+    body?: BodyInit | null;
+    withCredentials?: boolean;
+}
+
+/**
+ * @description EventSourcePolyfill onopen 이벤트
+ */
+export interface IEventSourcePolyfillOpenEvent extends Event {
+    status?: number;
+    statusText?: string;
+    [key: string]: any;
+}
+
+/**
+ * @description EventSourcePolyfill onmessage 이벤트
+ */
+export interface IEventSourcePolyfillMessageEvent extends MessageEvent {
+    [key: string]: any;
+}
+
+/**
+ * @description EventSourcePolyfill onerror 이벤트
+ */
+export interface IEventSourcePolyfillErrorEvent extends Event {
+    status?: number;
+    statusText?: string;
+    isJsonResponse?: boolean;
+    data?: string;
+    [key: string]: any;
+}
+
+/**
+ * @description EventSourcePolyfill 인스턴스 구조
+ */
+export interface IEventSourcePolyfill {
+    readyState: number;
+    onopen?: (event: IEventSourcePolyfillOpenEvent) => void | null;
+    onmessage?: (event: IEventSourcePolyfillMessageEvent) => Promise<void> | void | null;
+    onerror?: (event: IEventSourcePolyfillErrorEvent) => void | null;
+    close(): void;
+}
