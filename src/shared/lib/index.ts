c04619f0e335e40fbed6db9a17cd06636f780431
조현재 <rickyHJ1994@gmail.com>
2026-05-12 18:05:50 +0900

    feat(lib): expose util default objects via index

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,1 +1,14 @@
 // Utility functions
+export { default as httpUtil } from './http';
+export { default as formatUtil } from './format';
+export { default as browserUtil } from './browser';
+export { default as colorUtil } from './color';
+export { default as consoleUtil } from './console';
+export { default as cookieUtil } from './cookie';
+export { default as dateUtil } from './date';
+export { default as downloadUtil } from './download';
+export { default as encryptUtil } from './encrypt';
+export { default as fullScreenUtil } from './fullScreen';
+export { default as mathUtil } from './math';
+export { copyToClipboard } from './copy';
+export * as regex from './constants/regex';
